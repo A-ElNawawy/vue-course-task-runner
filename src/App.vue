e7116038e30e2858d501db03aaa6a6cef98bdcfd
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-09-01 20:26:06 +0200

    from initialization to CRUD

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,114 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header @show-add-task="toggleAddTask" title="Task Tracker" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks
+      @delete-task="deleteTask"
+      @toggle-reminder="toggleReminder"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Tasks from "./components/tasks.vue";
+import AddTask from "./components/AddTask.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header, Tasks, AddTask },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    async addTask(task) {
+      const res = await fetch(`api/tasks`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      if (!res.ok) {
+        alert("Error creating task");
+        return;
+      }
+
+      const data = await res.json();
+      this.tasks.push(data);
+    },
+    async fetchTasks() {
+      const res = await fetch("api/tasks");
+      const data = await res.json();
+      return data;
+    },
+    deleteTask(id) {
+      const deletedTask = this.tasks.find((task) => task.id === id);
+      if (confirm(`Delete ${deletedTask.text}?`)) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      const toggledTask = this.tasks.find((task) => task.id === id);
+      toggledTask.reminder = !toggledTask.reminder;
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task
+      @delete-task="$emit('delete-task', task.id)"
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import Task from "./task.vue";
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: Object,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
